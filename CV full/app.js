6460aa510088524a891ecff89af7893592c5adaf
HolaViru <gaston.toscanocv@gmail.com>
2023-02-25 19:20:36 -0300

    Cv full terminado

---
+++ CV full/app.js
@@ -0,0 +1,97 @@
+// MENU LATERAL
+var menu_visible = false;
+let menu = document.getElementById("nav");
+function mostrarOcultarMenu(){
+    if(menu_visible==false){//si esta oculto
+    menu.style.display = "block";
+    menu_visible = true;
+    }
+    else{
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
+//oculto el menu una vez que seleciono una opción
+let links = document.querySelectorAll("nav a");
+for(var x = 0; x <links.length;x++){
+    links[x].onclick = function(){
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
+
+//Creo las barritas de una barra particular indentificada por su id
+function crearBarra(id_barra){
+    for(i=0;i<=16;i++){
+        let div =document.createElement("div");
+        div.className = "e";
+        id_barra.appendChild(div);
+    }
+}
+
+//selecciono todas las barras generales para luego manipularlas
+let html = document.getElementById("html");
+crearBarra(html);
+let javascript = document.getElementById("javascript");
+crearBarra(javascript);
+let wordpress = document.getElementById("wordpress");
+crearBarra(wordpress);
+let photoshop = document.getElementById("photoshop");
+crearBarra(photoshop);
+let php = document.getElementById("php");
+crearBarra(php);
+let ilustrator = document.getElementById("ilustrator");
+crearBarra(ilustrator);
+
+//AHORA VOY A GUARDAR LA CANTIDAD DE BARRITAS QUE SE VA A IR PINTANDO POR CADA BARRA
+//Para eso utilizo un arreglo, cada posicion pertenece a un elemento
+//comienzan en -1 por que no tiene ninguna pintada
+let contadores = [-1,-1,-1,-1,-1,-1]
+//esta variable la voy a utilizar de bandera para saber si ya ejecuto la animacion
+let entro = false;
+
+//función que aplica las animaciones de la habilidad
+function efectoHabilidades(){
+    var habilidades = document.getElementById ("habilidades");
+    var distancia_skills = window.innerHeight - habilidades.getBoundingClientRect().top;
+    if(distancia_skills>=300 && entro==false){
+        entro = true;
+        const intervalHtml = setInterval(function(){
+            pintaBarra(html, 16, 0, intervalHtml);
+        },100);
+        const intervalJavascript = setInterval(function(){
+            pintaBarra(javascript, 14, 1, intervalJavascript);
+        },100);
+        const intervalWordpress = setInterval(function(){
+            pintaBarra(wordpress, 15, 2, intervalWordpress);
+        },100);
+        const intervalPhotoshop = setInterval(function(){
+            pintaBarra(photoshop, 9, 3, intervalPhotoshop);
+        },100);
+        const intervalPhp = setInterval(function(){
+            pintaBarra(php, 14, 4, intervalPhp);
+        },100);
+        const intervalIlustrator = setInterval(function(){
+            pintaBarra(ilustrator, 14, 5, intervalIlustrator);
+        },100);
+    }
+
+}
+
+//lleno una barra particular con la cantidad indicada
+function pintaBarra(id_barra, cantidad, indice, interval){
+    contadores[indice]++;
+    x = contadores[indice];
+    if(x < cantidad){
+        let elementos = id_barra.getElementsByClassName("e");
+        elementos[x].style.backgroundColor = "#299ebb";
+    }else{
+        clearInterval(interval)
+
+    }
+}
+
+//detecto el scrolling del mause para aplicar la animación de la barra
+window.onscroll = function(){
+    efectoHabilidades();
+}
